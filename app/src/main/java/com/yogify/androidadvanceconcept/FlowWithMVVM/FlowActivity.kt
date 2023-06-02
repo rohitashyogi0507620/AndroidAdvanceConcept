88e4615b31e6a0b824b7ff7aecfb1501cb1ae2b2
RohitashYogi <rohitash.yogi@probusinsurance.com>
2023-06-02 14:07:42 +0530

    Data Store

--- app/src/main/java/com/yogify/androidadvanceconcept/FlowWithMVVM/FlowActivity.kt
+++ app/src/main/java/com/yogify/androidadvanceconcept/FlowWithMVVM/FlowActivity.kt
@@ -1,12 +1,23 @@
 package com.yogify.androidadvanceconcept.FlowWithMVVM
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.lifecycleScope
 import com.yogify.androidadvanceconcept.databinding.ActivityFlowBinding
+import com.yogify.androidadvanceconcept.utils.PreferencesKeys.AGE
+import com.yogify.androidadvanceconcept.utils.PreferencesKeys.APPLICATION_PREFERENCES
+import com.yogify.androidadvanceconcept.utils.PreferencesKeys.NAME
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -16,6 +27,12 @@ class FlowActivity : AppCompatActivity() {
 
     private val mainViewModel: MainViewModel by viewModels()
 
+    private val dataStore by preferencesDataStore(name = APPLICATION_PREFERENCES)
+
+    var  count  =0
+
+    lateinit var userNameFlow: Flow<String>
+    lateinit var  userAgeFlow: Flow<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,5 +59,41 @@ class FlowActivity : AppCompatActivity() {
             }
         }
 
+
+        binding.btnStoredata.setOnClickListener {
+            count++
+            lifecycleScope.launch(Dispatchers.IO) {
+                storeUserInfo(10, "Rohitash Yogi ${count}")
+                Log.d("DATA", "Data Stored")
+            }
+        }
+
+        binding.btnFetcdata.setOnClickListener {
+
+        }
+
+        userAgeFlow= dataStore.data.map { preferences ->
+            preferences[AGE] ?: 0
+        }
+
+        userNameFlow = dataStore.data.map { preferences ->
+            preferences[NAME] ?: ""
+        }
+
+        lifecycleScope.launch(Dispatchers.Main){
+            userNameFlow.collect {
+                binding.textView.setText(it)
+
+            }
+        }
+
+
+    }
+
+    suspend fun storeUserInfo(age: Int, name: String) {
+        dataStore.edit { preferences ->
+            preferences[AGE] = age
+            preferences[NAME] = name
+        }
     }
 }
